870b6cca392a06584e2d330d5dd048dc615e5fe7
burovytsky <esfero1505@gmail.com>
2020-03-21 15:48:37 +0200

    1. Конструктор по умолчанию.[#247217]

---
+++ src/main/java/ru/job4j/tracker/oop/Animal.java
@@ -0,0 +1,12 @@
+package ru.job4j.tracker.oop;
+
+public class Animal {
+    public Animal() {
+        super();
+        System.out.println("Load Animal");
+    }
+
+    public static void main(String[] args) {
+        Tiger tiger = new Tiger();
+    }
+}
---
+++ src/main/java/ru/job4j/tracker/oop/Tiger.java
@@ -0,0 +1,8 @@
+package ru.job4j.tracker.oop;
+
+public class Tiger extends Predator {
+    public Tiger() {
+        super();
+        System.out.println("Load Tiger");
+    }
+}
