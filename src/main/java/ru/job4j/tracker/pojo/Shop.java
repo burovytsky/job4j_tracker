d918d3ea45dc45c6f978ae7018dfc9c71ef8702d
Constantine Burovytsky <esfero1505@gmail.com>
2020-03-23 13:16:01 +0200

    3. Удаление моделей из массива.[#247227]

---
+++ src/main/java/ru/job4j/tracker/pojo/Shop.java
@@ -0,0 +1,57 @@
+package ru.job4j.tracker.pojo;
+
+public class Shop {
+    public static void main(String[] args) {
+        Shop shop = new Shop();
+        Product[] products = new Product[5];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 4);
+        products[2] = new Product("Egg", 19);
+        products[3] = new Product("Sugar", 5);
+        products[4] = new Product("Ice cream", 10);
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+        System.out.println();
+        System.out.println("Удаляем значение из ячейки с индексом 1");
+        //удаляем значение из ячейки с индексом 1
+        shop.delete(products, 1);
+
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+        System.out.println();
+        System.out.println("Записываем  в ячейку с индексом 1 значение ячейки с индексом 2 и удаляем значение из ячейки с индексом 2");
+        //удаляем значение из ячейки с индексом 2.
+        shop.delete(products, 2);
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+    }
+
+    public Product[] delete(Product[] products, int index) {
+        products[index] = null;
+        for (; index < products.length - 1; index++) {
+            products[index] = products[index + 1];
+        }
+        products[products.length - 1] = null;
+        return products;
+    }
+}
