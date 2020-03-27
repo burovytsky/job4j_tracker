a30055d68390194c568dc0ad49e24062759db9a2
burovytsky <esfero1505@gmail.com>
2020-03-27 18:43:47 +0200

    9.1 Рефакторинг теста @Before @After[#247164]

--- src/main/java/ru/job4j/tracker/strategy/PaintTest.java
+++ src/main/java/ru/job4j/tracker/strategy/PaintTest.java
@@ -1,36 +1,60 @@
 package ru.job4j.tracker.strategy;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+
 import java.util.StringJoiner;
 
 public class PaintTest {
+    PrintStream stdout = System.out;
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
     @Test
     public void whenDrawSquare() {
-        // получаем ссылку на стандартный вывод в консоль.
-        PrintStream stdout = System.out;
-        // Создаем буфур для хранения вывода.
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        //Заменяем стандартный вывод на вывод в пямять для тестирования.
-        System.setOut(new PrintStream(out));
-        // выполняем действия пишушиее в консоль.
         new Paint().draw(new Square());
-        // проверяем результат вычисления
         assertThat(
                 new String(out.toByteArray()),
-                is(
-                        new StringJoiner(System.lineSeparator())
-                                .add("+++++++")
-                                .add("+     +")
-                                .add("+     +")
-                                .add("+++++++")
-                                .add(System.lineSeparator())
-                                .toString())
+                is(new StringJoiner(System.lineSeparator())
+                        .add("+++++++")
+                        .add("+     +")
+                        .add("+     +")
+                        .add("+++++++")
+                        .add(System.lineSeparator())
+                        .toString())
         );
-        // возвращаем обратно стандартный вывод в консоль.
-        System.setOut(stdout);
+    }
+
+    @Test
+    public void whenDrawTriangle() {
+        new Paint().draw(new Triangle());
+        assertThat(
+                new String(out.toByteArray()),
+                is(new StringJoiner(System.lineSeparator())
+                        .add("   *")
+                        .add("  ***")
+                        .add(" *****")
+                        .add("*******")
+                        .add(System.lineSeparator())
+                        .toString())
+        );
     }
 }
