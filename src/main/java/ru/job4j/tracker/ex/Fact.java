076cf78e6095e189e64dd15b33e0e7bff5f2dbca
Constantine Burovytsky <esfero1505@gmail.com>
2020-03-30 08:58:01 +0300

    0.3. Тестирование исключений с junit[#247186]

--- src/main/java/ru/job4j/tracker/ex/Fact.java
+++ src/main/java/ru/job4j/tracker/ex/Fact.java
@@ -1,13 +1,7 @@
 package ru.job4j.tracker.ex;
 
 public class Fact {
-    public static void main(String[] args) {
-        Fact fact = new Fact();
-        int rsl = fact.calc(-6);
-        System.out.println(rsl);
-    }
-
-    public int calc(int n) {
+    public static int calc(int n) {
         if (n < 0) {
             throw new IllegalArgumentException("Number must be greater than zero");
         }
---
+++ src/test/java/ru/job4j/tracker/FactTest.java
@@ -0,0 +1,11 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+import ru.job4j.tracker.ex.Fact;
+
+public class FactTest {
+    @Test(expected = IllegalArgumentException.class)
+    public void whenNIsLessThanZero() {
+        Fact.calc(-5);
+    }
+}
