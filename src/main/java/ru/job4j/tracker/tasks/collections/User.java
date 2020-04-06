d021a93e51b9262689c5c5338194e2ebe83ad78f
Constantine Burovytsky <esfero1505@gmail.com>
2020-04-06 11:21:19 +0300

    1. Организовать сортировку User[#247132]

---
+++ src/main/java/ru/job4j/tracker/tasks/collections/User.java
@@ -0,0 +1,46 @@
+package ru.job4j.tracker.tasks.collections;
+
+import java.util.Objects;
+
+public class User implements Comparable<User> {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(User o) {
+        int result = this.name.compareTo(o.name);
+
+        if (result == 0) {
+            if (this.age > o.age) {
+                result = 1;
+            } else if (this.age < o.age) {
+                result = -1;
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age
+                && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public String toString() {
+        return name + " - " + age;
+    }
+}
