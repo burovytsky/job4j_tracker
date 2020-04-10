089aebfda46e2140d833eb5f0717b7d67901aa25
burovytsky <esfero1505@gmail.com>
2020-04-10 19:20:52 +0200

    Отсортировать департаменты[#247127] удаление ненужного кода

--- src/main/java/ru/job4j/tracker/tasks/collections/DepDescComp.java
+++ src/main/java/ru/job4j/tracker/tasks/collections/DepDescComp.java
@@ -9,12 +9,6 @@ public class DepDescComp implements Comparator<String> {
         String[] o2Array = o2.split("/");
         int rsl = o2Array[0].compareTo(o1Array[0]);
         if (rsl == 0) {
-            for (int i = 1; i < Math.min(o1Array.length, o2Array.length); i++) {
-                rsl = o1Array[i].compareTo(o2Array[i]);
-                break;
-            }
-        }
-        if (rsl == 0) {
             rsl = o1.compareTo(o2);
         }
         return rsl;
