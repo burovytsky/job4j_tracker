69010f43f0640325eebd7a06615957a5e315e4e8
burovytsky <esfero1505@gmail.com>
2020-04-13 20:46:28 +0200

    0. Stream API улучшения[#247014] изменил еще раз compareTo

--- src/main/java/ru/job4j/tracker/tasks/lambda/Student.java
+++ src/main/java/ru/job4j/tracker/tasks/lambda/Student.java
@@ -39,6 +39,6 @@ public class Student implements Comparable<Student> {
 
     @Override
     public int compareTo(Student o) {
-        return Integer.compare(this.getScore(), o.getScore());
+        return Integer.compare(o.getScore(), this.getScore());
     }
 }
