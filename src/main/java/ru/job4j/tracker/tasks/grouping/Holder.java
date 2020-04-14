b738367d701483bf7432c6918d24f290c9298214
burovytsky <esfero1505@gmail.com>
2020-04-14 20:53:43 +0200

    Группировка по интересам. [#106993]

---
+++ src/main/java/ru/job4j/tracker/tasks/grouping/Holder.java
@@ -0,0 +1,10 @@
+package ru.job4j.tracker.tasks.grouping;
+
+public class Holder {
+    String key, value;
+
+    Holder(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+}
